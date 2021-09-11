461224d6395cf070dbcc6ef911ece48041dcfc05
Ethan Pang <ethanp1138@gmail.com>
2021-09-11 14:07:12 -0700

    add loading text for album photos

--- src/components/PhotoAlbum.jsx
+++ src/components/PhotoAlbum.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 function PhotoAlbum() {
 	
 	const [images, setImages] = useState([])
+	const [isLoading, setLoading] = useState(true)
 
 	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/photos")
@@ -15,33 +16,26 @@ function PhotoAlbum() {
 					temp.push(data[i]["url"])
 				}
 				setImages(temp)
+				setLoading(false)
 				console.log(images)
 			})
 	}, [])
 
-
-	// var pics = []
-	// fetch("https://jsonplaceholder.typicode.com/photos")
-	// 	.then(response => response.json())
-	// 	.then(data => {
-	// 		for (let i = 0; i < 24; i++) {
-	// 			console.log(data[i])
-	// 			pics.push(data[i]["url"]
-	// 				// <img src={data[i]["url"]}></img>
-	// 			)
-	// 		}
-	// 		console.log(pics)
-	// 	}).then(
-			
-	// 	)
-
 	return (
 	  <div className="PhotoAlbum">
 		  <h1>Photo Album</h1>
-		  <div className="dashboard-grid">
-			{images.map(url => {
-				return <img src={url} className="album-photos"/>;
-			})}
+		  <div className="album-grid">
+			  {isLoading ? (
+				  <>
+				  <p>Loading cute cat images</p>
+				  </>
+			  ) : (
+				  <>
+				{images.map(url => {
+					return <img src={url} className="album-photos"/>;
+				})}
+				</>
+			  )}
 		  </div>
 	  </div>
 	);
